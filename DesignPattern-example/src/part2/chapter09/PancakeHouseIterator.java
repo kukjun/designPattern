3c108c816ae1f3deb471fd1db46eadc3820339d1
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2021-08-16 15:35:40 +0900

    Upload chapter09~
    Chapter08장을 마무리하고 Chapter09장을 진행하고 있습니다.

---
+++ DesignPattern-example/src/part2/chapter09/PancakeHouseIterator.java
@@ -0,0 +1,30 @@
+package part2.chapter09;
+
+import java.util.ArrayList;
+
+public class PancakeHouseIterator implements Iterator {
+
+  ArrayList items;
+  int index = 0;
+
+  public PancakeHouseIterator(ArrayList items) {
+    this.items = items;
+  }
+
+  @Override
+  public boolean hasNext() {
+    if (index >= items.size() || items.get(index) == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @Override
+  public Object next() {
+    MenuItem menuItem = (MenuItem) items.get(index);
+    index++;
+    return menuItem;
+  }
+
+}
